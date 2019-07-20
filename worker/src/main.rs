fe30198fd73ddb76935a8adf6cc487b44aaafdd9
Alexey Erzyamkin <alexey.erzyamkin@outlook.com>
2019-07-20 11:18:12 +0300

    Implementing worker using reqwest

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,23 +1,37 @@
-use actix_rt::System;
-use actix_web::client::Client;
+// use actix_rt::System;
+// use actix_web::client::Client;
+
+use reqwest;
 
 use futures::{
     Future,
     lazy
 };
 
-fn main() {
-    System::new("worker").block_on(lazy(|| {
-        let client = Client::default();
+fn main() -> std::result::Result<(), Box<::std::error::Error>> {
+    // System::new("worker").block_on(lazy(|| {
+    //     let client = Client::default();
 
-        client
-            .get("http://127.0.0.1:8081/bot-runner/state")
-            .send()
-            .map_err(|_| ())
-            .and_then(|response| {
-                println!("R: {:?}", response);
+    //     client
+    //         .get("http://127.0.0.1:8081/bot-runner/state")
+    //         .send()
+    //         .map_err(|_| ())
+    //         .and_then(|response| {
+    //             println!("R: {:?}", response);
 
-                Ok(())
-            })
-    })).unwrap();
+    //             Ok(())
+    //         })
+    // })).unwrap();
+
+    let response_text = reqwest::get("http://127.0.0.1:8081/bot-runner/state")?.text()?;
+
+    println!("R: {}", response_text);
+
+    Ok(())
+}
+
+fn update_status() {
+    match reqwest::get("http://127.0.0.1:8081/bot-runner/state") {
+        
+    }
 }
